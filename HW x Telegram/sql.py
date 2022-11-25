ef101d26b05c44b8cbf1a53f53fa283a1beaba0e
artmen57 <80320472+artmen57@users.noreply.github.com>
2022-11-26 00:46:06 +0300

    Delete HW x Telegram directory

--- HW x Telegram/sql.py
+++
@@ -1,9 +0,0 @@
-import MySQLdb
-def quer(conn):
-	cur=conn.cursor()
-	cur.execute("SELECT count(*) FROM sakila.film_actor;")
-	for val in cur.fetchall():
-		print(val)
-cnx= MySQLdb.connect(host='localhost',user='root',passwd='357951',db='sakila')
-quer(cnx)
-cnx.close
